f0663285fb3b1e1f2326551438d76013cfb87e0e
Karthig <karthig@karthig-HP-Mini-5101.(none)>
2013-03-23 10:41:29 -0400

    Can parse tweets for URL and open them.  Need to capture newline chars from JSON

--- scripts.js
+++ scripts.js
@@ -30,14 +30,63 @@ $("#btn1").click(function(){
 		var tweets = [];
 
 		for(var i in data) {
-			tweets.push("<li>" +
-						"<a href='http://www.imdb.com' data-icon='star' data-theme='a'>" + 
-						"<img src=" + data[i].user.profile_image_url_https + ">" +
-						"<div data-role='collapsible'>" +
-						"<h3>" + data[i].user.name + "</h3>" +
-						"<p>" +	data[i].text + "</p></div></a></li>");
+
+			var ref_website = parseTweet(data[i].text);
+			console.log(ref_website);
+
+			if(ref_website==null) {
+				tweets.push("<li>" + 
+							"<img src=" + data[i].user.profile_image_url_https + ">" +
+							"<div data-role='collapsible'>" +
+							"<h3>" + data[i].user.name + "</h3>" +
+							"<p>" +	data[i].text + "</p></div></li>");
+			} else {
+				tweets.push("<li>" + 
+							"<a href='" + ref_website + "' data-icon='star' target='_blank' data-theme='a'>" +
+							"<img src=" + data[i].user.profile_image_url_https + ">" +
+							"<div data-role='collapsible'>" +
+							"<h3>" + data[i].user.name + "</h3>" +
+							"<p>" +	data[i].text + "</p></div></a></li>");
 			}
+
+		}
 		
 	$("#tweetlist").append(tweets.join('')).listview('refresh');					
 	});
 });
+
+function parseTweet(tweet){
+
+	var site;
+	//Get index of "http:"
+	var indexA = tweet.search("http:"); //Need to be http || https
+
+	if(indexA==-1){
+	
+		site = null;
+
+	} else {
+	
+		//Splice string "tweet" from http
+		var substr = tweet.slice(indexA,tweet.length);
+
+		//Get index of empty space
+		console.log("A: " + tweet);
+		console.log("B:" + substr.indexOf(" "));
+		console.log("C:" + substr.indexOf("\\"));
+
+		//Problem: Cannot grab newline character in tweet from SarahPrevette
+		//If you can grab it, you must still ensure it isn't -1 before taking min
+
+//		var indexB = Math.min(substr.indexOf(" "),substr.indexOf("\\"));
+		var indexB = substr.indexOf(" ");
+
+		if(indexB==-1){
+			site = substr;
+		} else {
+			site = substr.slice(0,indexB);
+		}
+	}
+
+	return site;
+}
